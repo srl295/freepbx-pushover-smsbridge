90fb331d59a96a5a987e9ac232aa9e89ce4b704d
Steven R. Loomis <srl295@gmail.com>
2022-09-07 15:01:19 -0500

    feat: MVP, works

--- README.md
+++ README.md
@@ -1,6 +1,48 @@
 # freepbx-pushover-smsbridge
 
-A bridge between FreePBX SMS and Pushover
+A bridge between [FreePBX SMS](https://freepbx.org) and Pushover
+
+## Setup
+
+1. Create a Pushover account
+2. Set up the [SMS Webhook](https://wiki.freepbx.org/display/FPG/SMS+Webhook) to be something such as <http://yourhost.example.com:21439> ( see 'port' setting below.) I recommend running this locally only.
+3. Create the file `config.json`. ("//" entries are comments.)
+
+```json
+{
+    "port": 21439,
+    "routing": {
+        "default": {
+            "user": "<pushover user key or group key for default routing>"
+        },
+        "12125551212": {
+            "user": "<pushover user key or group key>"
+        },
+        "18185551212": {
+            "user": "<pushover user key or group key>"
+        }
+    },
+    "PUSHOVER_USER": "<pushover user to be used as a last resort>",
+    "PUSHOVER_TOKEN": "…"
+}
+```
+
+If `PUSHOVER_TOKEN` is not set, they will be pulled from the environment. See [pushover-notifications](https://www.npmjs.com/package/pushover-notifications)
+
+## Routing
+
+Update the 'routing' part of config.json to custom route messages.
+
+## Sample input from Sipstation
+
+```json
+{ "to": "18185551212",
+  "from": "12125551212",
+  "adaptor": "Sipstation",
+  "time": "Wed, 31 Dec 1969 18:00:00 -0600",
+  "message": "Hello, world!",
+  "eventDirection": "in" }
+```
 
 ## License
 
--- index.js
+++ index.js
@@ -3,4 +3,77 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+const http = require('http');
+const Push = require('pushover-notifications');
+const package = require('./package.json');
+const config = require('./config.json');
+const process = require('process');
+
 console.log('Hello');
+
+const port = config.port || 21439;
+const host = config.host || '127.0.0.1';
+
+const server = http.createServer((req, res) => {
+    console.log(`${new Date().toISOString()} ${req.socket.address().address} ${req.method} ${req.url}`);
+    if (req.method !== 'POST') {
+        res.writeHead(405);
+        res.end(`${req.method} not allowed!\n`);
+    } else {
+        let data = "";
+        req.on("data", (d) => data += d);
+        req.on("end", () => {
+            try {
+                const o = JSON.parse(data);
+                console.dir(o);
+                res.writeHead(200, { "Content-Type": "text/plain" });
+                // res.end(`You wrote: ${JSON.stringify(o)}\n`);
+
+                const {to, from, adaptor, time, message, eventDirection} = o;
+
+                if (eventDirection == 'in') {
+                    let routing = config.routing[to];
+                    if (!routing) {
+                        routing = config.routing.default;
+                        console.log('Using default routing');
+                    }
+                    if (!routing) {
+                        console.error(`No routing for ${to} - default`);
+                        routing = { user: config.PUSHOVER_USER };
+                    }
+
+                    const msg = {
+                        title: `${from}»${to}`,
+                        message: `${message}`,
+                    };
+                    const p = new Push( {
+                        token:  config.PUSHOVER_TOKEN || process.env['PUSHOVER_TOKEN'],
+                        user: routing.user,
+                    });
+                    console.dir({msg, user: routing.user});
+                    // TODO: should ideally not do this inside of the request
+                    p.send(msg, (err, result) => {
+                        if(err) {
+                            res.end("err");
+                            throw err;
+                        }
+                        console.log(result);
+                        res.end("ok");
+                    })
+                } else {
+                    console.error('Skipping: ' + eventDirection);
+                    res.end("ok");
+                }
+            } catch (e) {
+                console.error(e);
+                console.log(data);
+                res.writeHead(200, { "Content-Type": "text/plain" });
+                res.end(`Error deseralizing data\n`);
+            }
+        });
+    }
+});
+server.listen(port, host, () => {
+    console.log(`${package.name}: listening to http://${host}:${port}`);
+});
+
